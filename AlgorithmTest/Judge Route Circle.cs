48ae920e185c946393cb7ac2f7f95bba9951fd9c
jun <jun.huang@shoplitics.cn>
2018-01-02 18:43:27 +0800

    Judge_Route_Circle

---
+++ AlgorithmTest/Judge Route Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Judge_Route_Circle
+    {
+        public static bool JudgeCircle(string moves)
+        {
+            int v = 0, h = 0;
+            foreach (var c in moves)
+            {
+                switch (c)
+                {
+                    case 'U':
+                        v++;
+                        break;
+                    case 'D':
+                        v--;
+                        break;
+                    case 'L':
+                        h++;
+                        break;
+                    case 'R':
+                        h--;
+                        break;
+                }
+            }
+            if (v == 0 && h == 0) return true;
+            return false;
+        }
+    }
+}
