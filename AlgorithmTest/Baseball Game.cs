746d67012181c5d5c54217616fc5e07016a6c9de
jun <jun.huang@shoplitics.cn>
2018-01-04 11:54:20 +0800

    Baseball Game

---
+++ AlgorithmTest/Baseball Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Baseball_Game
+    {
+        public static int CalPoints(string[] ops)
+        {
+            int ret = 0;
+            List<int> valid = new List<int>();
+            var tmp = 0;
+            foreach (var s in ops)
+            {
+                switch (s)
+                {
+                    case "+":
+                        tmp = valid[valid.Count - 1] + valid[valid.Count - 2];
+                        ret += tmp;
+                        valid.Add(tmp);
+                        break;
+                    case "D":
+                        tmp = valid[valid.Count - 1] * 2;
+                        ret += tmp;
+                        valid.Add(tmp);
+                        break;
+                    case "C":
+                        ret -= valid[valid.Count - 1];
+                        valid.RemoveAt(valid.Count-1);
+                        break;
+                    default:
+                        valid.Add(int.Parse(s));
+                        ret += valid[valid.Count - 1];
+                        break;
+                }
+            }
+            return ret;
+        }
+    }
+}
