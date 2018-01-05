db1d11205e3a278c143bbeb4d2c7b42cfe06711e
jun <jun.huang@shoplitics.cn>
2018-01-05 18:08:46 +0800

    Delete_(inputed)Node_in_a_Linked_List

---
+++ AlgorithmTest/Delete Node in a Linked List.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    /*
+    Write a function to delete a node(except the tail) in a singly linked list, given only access to that node.
+
+    Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+    */
+    class Delete_Node_in_a_Linked_List
+    {
+        // move each one forward
+        public static void DeleteNode(ListNode node)
+        {
+            while (node.next != null)
+            {
+                node.val = node.next.val;
+                if (node.next.next == null) break;
+                node = node.next;
+            }
+            node.next = null;
+        }
+
+        // simpler
+        public static void Solution2(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
