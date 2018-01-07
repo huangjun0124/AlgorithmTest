afe3e456d514dfabf29c0a6cfc0f4f21c3fb6bf0
jun <jun.huang@shoplitics.cn>
2018-01-07 17:47:59 +0800

    Binary Tree Paths

---
+++ AlgorithmTest/Binary Tree Paths.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    /*
+    Given a binary tree, return all root-to-leaf paths.
+
+    For example, given the following binary tree:
+        1
+      /   \
+     2     3
+      \
+       5
+    All root-to-leaf paths are:
+    ["1->2->5", "1->3"]
+    */
+    class Binary_Tree_Paths
+    {
+        List<string> ret = new List<string>();
+        public IList<string> BinaryTreePaths(TreeNode root)
+        {
+            if (root == null) return ret;
+            Traverse(root, "");
+            return ret;
+        }
+
+        private void Traverse(TreeNode node, string prefix)
+        {
+            if (node.left == null && node.right == null)
+            {
+                ret.Add(prefix + node.val);
+            }
+            if (node.left != null)
+            {
+                Traverse(node.left, prefix + node.val + "->");
+            }
+            if(node.right != null)
+            {
+                Traverse(node.right, prefix + node.val + "->");
+            }
+        }
+    }
+}
